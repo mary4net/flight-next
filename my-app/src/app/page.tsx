591be23cb18f5743361788d9b73877804d3b5c16
yoyo024 <126288329+yoyo024@users.noreply.github.com>
2025-03-31 15:17:50 -0400

    done part of fetch endpoints for booking

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -16,11 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-<<<<<<< HEAD
-              app/page.tsx
-=======
               src/app/page.tsx
->>>>>>> 217f1b930152cfabea074773f0bb11eb1e12e750
             </code>
             .
           </li>
